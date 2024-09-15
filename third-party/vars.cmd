93e3f73c0311b167da7ac4f13e02aa0255c890ad
Andrea Guerrieri <aguerrieri@eusoft.net>
2024-09-16 00:27:53 +0200

    native build scripts include native libs in source

---
+++ third-party/vars.cmd
@@ -0,0 +1,8 @@
+SET NDK_HOME=c:\Android\ndk\27.0.12077973\
+SET VC_HOME=C:\Program Files\Microsoft Visual Studio\2022\Preview\VC
+SET ANDROID_ABI=arm64-v8a
+SET ANDROID_PLATFORM=30
+SET ANDROID_STL=c++_static
+SET BUILD_TYPE=release
+SET INSTALL_PEFIX=install
+SET NASM_PATH="C:\msys64\usr\bin\nasm.exe"
