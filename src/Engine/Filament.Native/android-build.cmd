015e584bb038d410d64d91028b89c282adafdf71
Andrea Guerrieri <aguerrieri@eusoft.net>
2024-11-07 22:05:07 +0100

    fix native libs part-1

--- src/Engine/Filament.Native/android-build.cmd
+++ src/Engine/Filament.Native/android-build.cmd
@@ -3,3 +3,5 @@ call ..\..\..\third-party\vars.cmd
 SET NDK_PROJECT_PATH=%CD%
 
 call %NDK_HOME%\build\ndk-build NDK_DEBUG=0
+
+copy libs\arm64-v8a\*.so ..\..\..\libs\filament-native\android-arm64\
--- src/Engine/XrEngine.Core/Abstraction/IBehavior.cs
+++ src/Engine/XrEngine.Core/Abstraction/IBehavior.cs
@@ -3,7 +3,5 @@
     public interface IBehavior : IComponent, IRenderUpdate
     {
         IUpdateGroup? UpdateGroup { get; }
-
-        int UpdatePriority { get; }
     }
 }
