f08cd744f33a8a0673be7227d3d1d0d0b1c745cb
Andrea Guerrieri <aguerrieri@eusoft.net>
2026-05-26 19:13:20 +0200

    update runtime

--- third-party/vars.cmd
+++ third-party/vars.cmd
@@ -1,6 +1,6 @@
 if "%XR_BUILD_VARS%" == "1" exit /b
 
-SET NDK_HOME=c:\Android\Sdk\ndk\29.0.14206865\
+SET NDK_HOME=c:\Android\Sdk\ndk\30.0.14904198\
 SET VC_HOME=C:\Program Files\Microsoft Visual Studio\18\Insiders\VC
 
 SET ANDROID_ABI=arm64-v8a
