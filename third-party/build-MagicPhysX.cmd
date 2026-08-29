5b705dd6859768006e43459513a1866151932c67
Andrea Guerrieri <aguerrieri@eusoft.net>
2026-08-29 19:22:45 +0200

    px build update

--- third-party/build-MagicPhysX.cmd
+++ third-party/build-MagicPhysX.cmd
@@ -4,6 +4,9 @@ SET CUR_DIR=%CD%
 
 cd physx-rs\physx-sys\src
 
+
+REM Android
+
 md out-android
 cd out-android
 
@@ -14,11 +17,27 @@ cmake -G Ninja .. -DCMAKE_TOOLCHAIN_FILE=%NDK_HOME%\build\cmake\android.toolchai
 		 -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
 		 -DCMAKE_INSTALL_PREFIX=%INSTALL_PEFIX% ^
 		 -DANDROID=1
-	  
-ninja 
+
+ninja
 
 %LLVM_STRIP% --strip-unneeded libphysxnative.so
 
 copy libphysxnative.so %CUR_DIR%\..\libs\physxnative\android-arm64\libphysxnative.so
+
+cd ..
+
+
+REM Windows
+
+md out-win64
+cd out-win64
+
+cmake -G Ninja .. ^
+		 -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+		 -DCMAKE_INSTALL_PREFIX=%INSTALL_PEFIX%
+
+ninja
+
+copy physxnative.dll %CUR_DIR%\..\libs\physxnative\win-x64\physxnative.dll
 
 cd %CUR_DIR%
