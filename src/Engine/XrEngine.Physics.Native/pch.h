55ddb2b8595d292569331b203d3190d4f6b0c4d8
Andrea Guerrieri <aguerrieri@eusoft.net>
2026-08-30 06:15:56 +0200

    Update native vehicle bridge for PhysX 5.9

--- src/Engine/XrEngine.Physics.Native/pch.h
+++ src/Engine/XrEngine.Physics.Native/pch.h
@@ -28,20 +28,28 @@
 #include "extensions/PxRigidActorExt.h"
 #include "extensions/PxRigidBodyExt.h"
 
+#ifdef __ANDROID__
 #include "vehicle2/PxVehicleAPI.h"
+#else
+#include "vehicle/PxVehicleAPI.h"
+
+// PhysX 5.9 folded Vehicle2 into the main physx namespace. Keep the existing
+// bridge source compatible with the current Windows SDK spelling.
+namespace vehicle2 = physx;
+#endif
 
 #include "Vehicle.h"
 
 
 #ifdef _WINDOWS
 
-#define PHYSX_LIB_PATH "..\\..\\..\\libs\\physx-141\\bin\\win-x64\\mt\\release\\"
+#define PHYSX_LIB_PATH "..\\..\\..\\third-party\\physx-rs\\physx-sys\\physx\\physx\\lib\\bin\\win.x86_64.vc143.md\\release\\"
 
 #pragma comment(lib, PHYSX_LIB_PATH "PhysX_64.lib")
 #pragma comment(lib, PHYSX_LIB_PATH "PhysXCommon_64.lib")
 #pragma comment(lib, PHYSX_LIB_PATH "PhysXFoundation_64.lib")
 #pragma comment(lib, PHYSX_LIB_PATH "PhysXExtensions_static_64.lib")
 #pragma comment(lib, PHYSX_LIB_PATH "PhysXCooking_64.lib") 
-#pragma comment(lib, PHYSX_LIB_PATH "PhysXVehicle2_static_64.lib")
+#pragma comment(lib, PHYSX_LIB_PATH "PhysXVehicle_static_64.lib")
 
 #endif
